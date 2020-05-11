d66b25420f3c5152756364297aa2772494a81ce6
andjelam97 <65087283+andjelam97@users.noreply.github.com>
2020-05-11 16:03:39 +0200

    Add files via upload

---
+++ defines.php
@@ -0,0 +1,4 @@
+<?php
+    define('INSTAGRAM_APP_ID','725584818208638');
+    define('INSTAGRAM_APP_SECRET', 'e857adc85f01a10fef18b62670449bd4');
+    define('INSTAGRAM_APP_REDIRECT_URI','https://andjelam97.github.io/');
