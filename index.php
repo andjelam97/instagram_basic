2d9f509c8b028b8cf13f51ab49159d59da29db1b
andjelam97 <65087283+andjelam97@users.noreply.github.com>
2020-05-11 22:01:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Instagram Basic Display Api</h1>
+    
+    <?php
 	require_once( 'instagram_basic_display_api.php' );
 
 	
@@ -8,7 +18,12 @@
 		
 	);
 	$ig = new instagram_basic_display_api( $params );
-?>
-	<a href="<?php echo $ig->authorizationUrl; ?>">
+     
+    ?>
+    
+    <a href="<?php echo $ig->authorizationUrl; ?>">
 		Authorize w/Instagram
 	</a>
+
+</body>
+</html>
